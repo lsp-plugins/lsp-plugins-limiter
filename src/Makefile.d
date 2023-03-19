2486a4c21a8314a82138e22d8bfd6d2e0baa072e
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-19 15:20:45 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -415,6 +415,7 @@ $(LSP_PLUGINS_LIMITER_BIN)/main/ui/limiter.o: main/ui/limiter.cpp \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPlayListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ControlPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PathPort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ProxyPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Factory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIOverrides.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIContext.h \
